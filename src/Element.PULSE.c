10b003015d5ad53acfb6fcf3380eee8c7c82c352
John Hodge <tpg@mutabah.net>
2013-04-11 23:33:43 +0800

    Simulator - Added trailing edge pulse support

--- src/Element.PULSE.c
+++ src/Element.PULSE.c
@@ -6,10 +6,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct
 {
-	 int	CurVal;
+	bool	CurVal;
+	bool	Dir;
 }	t_info;
 
 // === CODE ===
@@ -18,7 +20,12 @@ static tElement *_Create(int NParams, int *Param, int NInputs)
 	tElement *ret = EleHelp_CreateElement(1, 1, sizeof(t_info));
 	if(!ret)	return NULL;
 	t_info *info = ret->Info;
-	
+
+	if( NParams && *Param )
+		info->Dir = 1;
+	else
+		info->Dir = 0;
+
 	info->CurVal = 0;
 	
 	return ret;
@@ -27,12 +34,22 @@ static tElement *_Create(int NParams, int *Param, int NInputs)
 static void _Update(tElement *Ele)
 {
 	t_info	*this = Ele->Info;
+	bool	nextval = GetLink(Ele->Inputs[0]);
 	
-	// Single pulse on rising edge
-	if( !this->CurVal && GetLink(Ele->Inputs[0]) ) {
-		RaiseLink(Ele->Outputs[0]);
+	if( this->Dir )
+	{
+		// Single pulse on trailing edge
+		if( this->CurVal & !nextval )
+			RaiseLink(Ele->Outputs[0]);
 	}
-	this->CurVal = !!GetLink(Ele->Inputs[0]);
+	else
+	{
+		// Single pulse on rising edge
+		if( !this->CurVal && nextval ) {
+			RaiseLink(Ele->Outputs[0]);
+		}
+	}
+	this->CurVal = nextval;
 }
 
 tElementDef gElement_PULSE = {
