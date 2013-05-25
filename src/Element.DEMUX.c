35b08d556a672018f21afb594225bbc5d3c33f22
John Hodge <tpg@mutabah.net>
2013-05-25 11:39:07 +0800

    Simulator - Added support for bus DEMUX

--- src/Element.DEMUX.c
+++ src/Element.DEMUX.c
@@ -8,39 +8,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdio.h>
+
+struct info
+{
+	 int	databits;
+};
 
 // === CODE ===
 static tElement *_Create(int NParams, int *Params, int NInputs)
 {
 	 int	bits = 1;
+	 int	databits = 1;
 
-	if( NParams != 1 )	return NULL;
+	if( NParams < 1 || NParams > 2 )	return NULL;
 	bits = Params[0];
+	if( NParams == 2 )
+		databits = Params[1]; 
 	
-	if( NInputs < 1 + bits ) {
+	if( NInputs < databits + bits ) {
+		fprintf(stderr, "DEMUX: Input count %i < expected %i", NInputs, databits + bits);
 		return NULL;
 	}
 	
-	tElement *ret = EleHelp_CreateElement(NInputs, 1 << bits, 0);
+	tElement *ret = EleHelp_CreateElement(NInputs, databits * (1 << bits), sizeof(struct info));
 	if(!ret)	return NULL;
+	struct info	*info = ret->Info;
+	info->databits = databits;
 	
 	return ret;
 }
 
 static void _Update(tElement *Ele)
 {
-	 int	val = 0;
+	struct info	*info = Ele->Info;
+	unsigned int	sel = 0;
 	
 	// Parse inputs as a binary stream
-	for( int i = 1; i < Ele->NInputs; i ++ )
+	for( int i = info->databits; i < Ele->NInputs; i ++ )
 	{
 		if( GetLink(Ele->Inputs[i]) )
-			val |= 1 << (i-1);
+			sel |= 1 << (i-info->databits);
 	}
 	
 	// If ENABLE, drive output
-	if( GetLink(Ele->Inputs[0]) )
-		RaiseLink(Ele->Outputs[val]);
+	for( int i = 0; i < info->databits; i ++ )
+	{
+		if( GetLink(Ele->Inputs[i]) )
+			RaiseLink(Ele->Outputs[sel*info->databits+i]);
+	}
 }
 
 tElementDef gElement_DEMUX = {
